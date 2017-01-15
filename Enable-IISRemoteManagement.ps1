0256b7d91dea2c99ed422b9104f697155e2c063f
PsPsam <pspsam@psam.se>
2017-01-15 20:26:49 +0100

    Initial Publish

---
+++ Enable-IISRemoteManagement.ps1
@@ -0,0 +1,79 @@
+﻿Function Test-WindowsFeature
+{
+    param (
+        $ComputerName,
+        $ServiceName
+    )
+    $getFeatureParams = @{
+        ComputerName = $ComputerName
+        Name = $ServiceName
+        ErrorAction = 'Ignore'
+    }
+    if ($feature = Get-Windowsfeature @getFeatureParams)
+    {
+        if ($feature.Installed) {
+            $true
+        } Else {
+            $false
+        }
+
+    }
+}
+
+Function Set-RemoteRegistryValue {
+    Params (
+        $ComputerName,
+        $Path,
+        $RegName,
+        $RegValue
+    )
+    $null = Invoke-Command -ComputerName $ComputerName -Scriptblock {Set-ItemProperty -path $useing:Path -Name $using:Name -Value $using:Value}
+}
+
+
+Function Enable-IISRemoteManagement
+{
+    [CmdletBinding()]
+    param
+    (
+        [Parameter(Mandatory)]
+        [ValidateScript({
+             if (-not (Test-Connection -ComputerName $_ -Quiet -Count 1)) {
+                 throw "The computer [$_] could not be reached."
+             } else {
+                 $true
+             }
+        })]
+        [ValidateLength(1,15)]
+        [string]$ComputerName
+    )
+
+    # Verify the IIS Management Service Windows feature is installed.
+    if (Test-WindowsFeature -ComputerName $ComputerName -Name 'Web-Mgmt-Service')
+    {
+        # Enable Remote Management via a registry key.
+        $remoteKeyParams = @{
+            ComputerName = $ComputerName
+            Path = 'HKLM:\SOFTWARE\Microsoft\WebManagement\Server'
+            Name = 'EnableRemoteManagement'
+            Value = '1'
+        }
+        Set-RemoteRegistryValue @remoteKeyParams
+
+        # Set the IIS Remote Management service to automatically start.
+        $setParams = @{
+            ComputerName = $ComputerName
+            Name = 'WMSvc'
+            StartupType = 'Automatic'
+        }
+        Set-Service @setParams
+
+        # Start the IIS Remote Management service.
+        Get-Service -ComputerName $ComputerName -Name 'WMSvc' | Start-Service
+    }
+    else
+    {
+        throw 'IIS Management Service Windows feature is not installed.'
+    }
+
+}
---
+++ Enable-IISRemoteManagement.tests.ps1
@@ -0,0 +1,83 @@
+﻿#Pester Script
+
+Describe 'Enable-IISRemoteManagement' {
+     context 'Input' {
+         it 'when the computer is offline, it will throw an exception' {
+ 
+             mock 'Test-Connection' {
+                 $false
+             }
+ 
+             { Enable-IISRemoteManagement -ComputerName 'IAMOFFLINE' } | should throw 'could not be reached'
+         }
+     }
+
+    context 'Execution' {
+        mock 'Test-Connection' {
+            $true
+        }
+    
+        mock 'Test-WindowsFeature' {
+            $false
+        }
+    
+        mock 'Set-RemoteRegistryValue'
+    
+        mock 'Set-Service'
+    
+        mock 'Start-Service'
+    
+        mock 'Get-Service'{
+            New-MockObject -Type 'System.ServiceProcess.ServiceController'
+        }
+        
+        it 'When the web-Mgmt-Service feature is installed, it changes the EnabledRemoteManagement reg value to 1' {
+            $null = Enable-IISRemoteManagement -ComputerName 'SOMETHING'
+
+            $assertMockParameters =  @{
+                CommandName = 'Set-RemoteRegistryValue'
+                Times = 1
+                Exactly = $True
+                ParameterFilter = { $ComputerName -eq 'SOMETHING'}
+            }
+            Assert-MockCalled @assertMockParameters
+        }
+        it 'When the web-Mgmt-Service feature is installed, it changes the WMSvc services starup type to Automatic' {
+            $null = Enable-IISRemoteManagement -ComputerName 'SOMETHING'
+            $assertMockParameters =  @{
+                CommandName = 'Set-Service'
+                Times = 1
+                Exactly = $True
+                ParameterFilter = { $ComputerName -eq 'SOMETHING'}
+            }
+            Assert-MockCalled @assertMockParameters
+        }
+        it 'When the web-Mgmt-Service feature is already installed, it starts the WMSvc service' {
+            $null = Enable-IISRemoteManagement -ComputerName 'SOMETHING'
+            $assertMockParameters =  @{
+                CommandName = 'Get-Service'
+                Times = 1
+                Exactly = $True
+            }
+            Assert-MockCalled @assertMockParameters -ParameterFilter {$ComputerName -eq 'SOMETHING'}
+            $assertMockParameters.CommandName = 'Start-Service'
+            Assert-MockCalled @assertMockParameters
+
+        }
+    }
+
+    context 'Output' {
+         it 'when the computer already has the Web-Mgmt-Service featureb installed , it will throw an exception' {
+ 
+             mock 'Test-Connection' {
+                 $true
+             }
+ 
+             mock 'Test-WindowsFeature' {
+                 $false
+             }
+ 
+             { Enable-IISRemoteManagement -ComputerName 'IAMONLINE' } | should throw 'Windows feature is not installed'
+         }
+     }
+}
