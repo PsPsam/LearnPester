88e6f82da57fe873bd822ec786859690e4c6a7f1
pspsam <pspsam@psam.se>
2017-05-26 09:31:25 +0200

    TestDrive

--- File/Remove-backupfile.Tests.ps1
+++ File/Remove-backupfile.Tests.ps1
@@ -1,9 +1,147 @@
-﻿$here = Split-Path -Parent $MyInvocation.MyCommand.Path
-$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
-. "$here\$sut"
+﻿#Dot source the ps1 file
+. .\Remove-backupfile.ps1
 
-Describe "Remove-backupfile" {
-    It "does something useful" {
-        $true | Should Be $false
+Describe "Check with Scriptanalyser" {
+  It 'must pass PSScriptAnalyzer rules' {
+    Invoke-ScriptAnalyzer -Path ".\Remove-backupfile.ps1" | should beNullOrEmpty
+  }
+}
+
+Describe "Remove-files" {
+
+  Context "folder has old and new files" {
+    #Setup -Dir "env"
+    #Setup -Dir "env\server"
+    Setup -Dir "env\server\Full"
+    Setup -Dir "env\server\Diff"
+    Setup -Dir "env\server\Log"
+    Setup -File "env\server\old.txt"
+    Setup -File "env\server\full\old.bak"
+    Setup -File "env\server\diff\old.bak"
+    Setup -File "env\server\log\old.bak"
+    # change the file creation date
+    $oldFile = Get-Item "TestDrive:\env\server\old.txt"
+    $oldFile.CreationTime = (Get-Date).AddDays(-3)
+    Setup -File "env\server1\new.txt"
+
+    It "new file exists" {
+      Test-Path "TestDrive:\env\server1\new.txt" | Should Be $true
     }
+    It "old file exists" {
+      Test-Path "TestDrive:\env\server\old.txt" | Should Be $true
+    }
+    It "removes the old files" {
+      Remove-file -path "TestDrive:env" -date (Get-Date).AddDays(-2)
+      Test-Path "TestDrive:\env\server\old.txt" | Should Be $false
+    }
+ 
+    It "leaves the new files" {
+      Remove-file -path "TestDrive:env" -date (Get-Date).AddDays(-2)
+      Test-Path "TestDrive:\env\server1\new.txt" | Should Be $true
+    }
+  }
+ 
+  Context "folders doesn't have old files" {
+    Setup -File "env\server\new1.txt" 
+    Setup -File "env\server\new2.txt" 
+ 
+    It "does nothing" {
+      Remove-file -path "TestDrive:" -date (Get-Date).AddDays(-2)
+      Test-Path "TestDrive:\env\server\new1.txt" | Should Be $true
+      Test-Path "TestDrive:\env\server\new2.txt" | Should Be $true
+    }
+  }
+}
+
+Describe "Remove-EmptyFolders" { 
+  Context "path has empty folders" {
+    Setup -Dir "env\empty"
+    It "removes the folder" {
+      Remove-EmptyFolder -path "TestDrive:\env"
+      Test-Path "TestDrive:\empty" | Should Be $false
+    }
+  }
+  Context "path does not have empty folders" {
+    Setup -Dir "env\notempty"
+    Setup -file "env\notempty\file.txt" 
+    It "does not removes the folder" {
+      Remove-EmptyFolder -path "TestDrive:notempty"
+      Test-Path "TestDrive:\env" | Should Be $true
+    }
+  }
+}
+
+Describe "Remove-OldFiles" {
+  Setup -Dir "env\folder"
+  #New-item "TestDrive:\new.txt" -Type file
+  # change the file creation date
+  #$oldFile = Get-Item "TestDrive:\old.txt"
+  #$oldFile.CreationTime = (Get-Date).AddDays(-3)
+  #New-item "TestDrive:\new.txt" -Type file
+
+  $today = get-date
+  mock Get-Date { $today }
+  mock Remove-File {}
+  mock Remove-EmptyFolder {}
+
+
+  Context "Input" {
+    It -name "When path doesn't exists, it will throw an exception" -test {
+      {Remove-OldFile -path 'TestDrive:\wrongfolder' -olderThan 3} | Should throw
+    }
+    
+    It "given folder should exist" {
+      {Remove-OldFile -path 'TestDrive:\env\folder' -olderThan 3} | Should be $true
+    }
+  }
+  
+  Context "Execution" {
+    #    $result = {Remove-OldFile -path 'TestDrive:\folder' -olderThan 3}
+
+    It "should get a date time" {
+      setup -Dir "env"
+      $null = Remove-OldFile -Path 'TestDrive:\env' -olderThan 3
+      
+      $assMParams = @{
+        CommandName = 'Get-Date'
+        Times       = 1
+        Exactly     = $true
+        Scope       = 'It'
+      }
+      Assert-MockCalled @assMParams
+    }
+    It "Should call Remove-File" {
+      $null = Remove-OldFile -Path 'TestDrive:\env' -olderThan 3
+      
+      $assMParams = @{
+        CommandName     = 'Remove-File'
+        Times           = 1
+        Exactly         = $true
+        Scope           = 'It'
+        ParameterFilter = {
+          $path -eq 'TestDrive:\env' -and 
+          $date -eq ($today).adddays(-3)
+        }
+      }
+      Assert-MockCalled @assMParams
+    }
+   
+    It "Should call Remove-EmptyFolder when empyfolder is true" {
+      $null = Remove-OldFile -Path 'TestDrive:\env' -olderThan 3 -emptyfolder $true
+      $assMParams = @{
+        CommandName     = 'Remove-EmptyFolder'
+        Times           = 1
+        Exactly         = $true
+        Scope           = 'It'
+        ParameterFilter = {
+          $path -eq 'TestDrive:\env'
+        }
+      }
+      Assert-MockCalled @assMParams
+    }
+  }
+
+  Context "Output" {
+
+  }
 }
--- File/Remove-backupfile.ps1
+++ File/Remove-backupfile.ps1
@@ -1,28 +1,199 @@
-﻿function Remove-backupfile {
-    [CmdletBinding()]
-    [Alias()]
-    Param
-    (
-        # Path to the folder to check for old files
-        [Parameter(Mandatory = $true,
-            ValueFromPipelineByPropertyName = $true,
-            Position = 0)]
-        [string]$path,
+﻿function Remove-File {
+  <#
+    .SYNOPSIS
+    Describe purpose of "Remove-File" in 1-2 sentences.
 
-        # Age of days to delete
-        [int]$age
-    )
+    .DESCRIPTION
+    Add a more complete description of what the function does.
 
-    Begin {
-        $checkdate = (get-date).AddDays( - $age)
+    .PARAMETER path
+    Describe parameter -path.
+
+    .PARAMETER date
+    Describe parameter -date.
+
+    .EXAMPLE
+    Remove-File -path Value -date Value
+    Describe what this call does
+
+    .NOTES
+    Place additional notes here.
+
+    .LINK
+    URLs to related sites
+    The first link is opened by Get-Help -Online Remove-File
+
+    .INPUTS
+    List of input types that are accepted by this function.
+
+    .OUTPUTS
+    List of output types produced by this function.
+  #>
+
+
+  [CmdletBinding(SupportsShouldProcess = $true, ConfirmImpact = "High")]
+  Param
+  (
+    # Path to the folder to check for old files
+    [Parameter(Mandatory, HelpMessage = 'Add help message for user',
+      ValueFromPipelineByPropertyName,
+      Position = 0)]
+    [string]$path,
+
+    # Older than days to delete
+    [Parameter(Mandatory, HelpMessage = 'Add help message for user',
+      ValueFromPipelineByPropertyName,
+      Position = 1)]
+    [datetime]$date
+  )
+
+  Begin {
+  }
+  Process {
+    # Delete files older than the $limit.
+    Write-Verbose -Message "Starting to check the given path $path for old files"
+    Get-ChildItem -Path $path -Recurse -Force |
+      Where-Object -FilterScript {
+      !$_.PSIsContainer -and $_.CreationTime -lt $date 
+    } |
+      Remove-Item -Force
+  }
+  End {
+  }
+}
+
+function Remove-EmptyFolder {
+  <#
+    .SYNOPSIS
+    Describe purpose of "Remove-EmptyFolder" in 1-2 sentences.
+
+    .DESCRIPTION
+    Add a more complete description of what the function does.
+
+    .PARAMETER path
+    Describe parameter -path.
+
+    .EXAMPLE
+    Remove-EmptyFolder -path Value
+    Describe what this call does
+
+    .NOTES
+    Place additional notes here.
+
+    .LINK
+    URLs to related sites
+    The first link is opened by Get-Help -Online Remove-EmptyFolder
+
+    .INPUTS
+    List of input types that are accepted by this function.
+
+    .OUTPUTS
+    List of output types produced by this function.
+  #>
+
+
+  [CmdletBinding(SupportsShouldProcess = $true, ConfirmImpact = "High")]
+  Param
+  (
+    # Path to the folder to check for old files
+    [Parameter(Mandatory, HelpMessage = 'Will remove empty subfolders',
+      ValueFromPipelineByPropertyName,
+      Position = 0)]
+    [string]$path
+  )
+
+  Begin {
+  }
+
+  Process {
+    # Delete files older than the $limit.
+    Write-Verbose -Message "Starting to check the given path $path for empty folders"
+    # Delete any empty directories left behind after deleting the old files.
+    Get-ChildItem -Path $path -Recurse -Force |
+      Where-Object {
+      $_.PSIsContainer -and $null -eq (Get-ChildItem -Path $_.FullName -Recurse -Force |
+          Where-Object {-not $_.PSIsContainer})
+    } |
+      Remove-Item -Force -Recurse
+  }
+  End {
+  }
+}
+
+function Remove-OldFile {
+  <#
+    .SYNOPSIS
+    Describe purpose of "Remove-OldFiles" in 1-2 sentences.
+
+    .DESCRIPTION
+    Add a more complete description of what the function does.
+
+    .PARAMETER path
+    Describe parameter -path.
+
+    .PARAMETER olderThan
+    Describe parameter -olderThan.
+
+    .PARAMETER empyfolder
+    Describe parameter -empyfolder.
+
+    .EXAMPLE
+    Remove-OldFiles -path Value -olderThan Value -empyfolder Value
+    Describe what this call does
+
+    .NOTES
+    Place additional notes here.
+
+    .LINK
+    URLs to related sites
+    The first link is opened by Get-Help -Online Remove-OldFiles
+
+    .INPUTS
+    List of input types that are accepted by this function.
+
+    .OUTPUTS
+    List of output types produced by this function.
+  #>
+
+
+  [CmdletBinding(SupportsShouldProcess = $true, ConfirmImpact = "High")]
+  Param
+  (
+    # Path to the folder to check for old files
+    [Parameter(Mandatory, HelpMessage = 'Path to folder to check for old files',
+      ValueFromPipelineByPropertyName,
+      Position = 0)]
+    [ValidateScript( {Test-Path -Path $_})]
+    [string]$path,
+
+    # Older than days to delete
+    [Parameter(Mandatory, HelpMessage = 'Age of the files to remove in days',
+      ValueFromPipelineByPropertyName,
+      Position = 1)]
+    [int]$olderThan,
+
+    # remove empty folders
+    [Parameter(ValueFromPipelineByPropertyName,
+      Position = 2)]
+    [Validateset($true, $false)]
+    [bool]$emptyfolder = $false
+  )
+
+  Begin {
+    #$checkdate = get-date
+    $checkdate = (get-date).AddDays( - $olderThan)
+  }
+  Process {
+    # Call the function to remove files
+    Remove-File -path $path -date $checkdate
+    Write-Output -InputObject 'Remove old files done'
+    # Call the function to remove empty folders if $emptyfolder -eq $true
+    if ($emptyfolder) {
+      Remove-EmptyFolder -path $path
+      Write-Output -InputObject 'Removed empty folders done'
     }
-    Process {
-        # Delete files older than the $limit.
-        write-verbose -Message "Starting to check the given path $path"
-        Get-ChildItem -Path $path -Recurse -Force | Where-Object { !$_.PSIsContainer -and $_.CreationTime -lt $checkdate } | Remove-Item -Force
-        # Delete any empty directories left behind after deleting the old files.
-        Get-ChildItem -Path $path -Recurse -Force | Where-Object { $_.PSIsContainer -and (Get-ChildItem -Path $_.FullName -Recurse -Force | Where-Object { !$_.PSIsContainer }) -eq $null } | Remove-Item -Force -Recurse
-    }
-    End {
-    }
+  }
+  End {
+    Write-Output -InputObject 'Done'
+  }
 }
